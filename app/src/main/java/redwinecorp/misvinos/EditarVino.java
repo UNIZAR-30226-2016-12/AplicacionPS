7e2b680986026dc53b6684c916663f386e7afe44
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-04-18 13:12:51 +0200

    Borrados prints sobrantes

--- app/src/main/java/redwinecorp/misvinos/EditarVino.java
+++ app/src/main/java/redwinecorp/misvinos/EditarVino.java
@@ -144,7 +144,6 @@ public class EditarVino extends AppCompatActivity {
                 p = Long.parseLong(localizacion.getText().toString());
                 a = Long.parseLong(year.getText().toString());
                 v = new Long(Math.round(valoracion.getRating()));
-                System.out.println(v);
             } catch (NumberFormatException e) {
                 valido=false;
             }
@@ -182,7 +181,6 @@ public class EditarVino extends AppCompatActivity {
                 p = Long.parseLong(localizacion.getText().toString());
                 a = Long.parseLong(year.getText().toString());
                 v = new Long(Math.round(valoracion.getRating()));
-                System.out.println(v);
             } catch (NumberFormatException e) {
                 valido=false;
             }
@@ -224,7 +222,6 @@ public class EditarVino extends AppCompatActivity {
         if(!devolver.equalsIgnoreCase("")){
             devolver = devolver.substring(0,devolver.length() - 2);
         }
-        System.out.println(devolver);
         return devolver;
     }
 
