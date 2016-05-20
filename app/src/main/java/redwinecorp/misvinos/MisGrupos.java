e0f80e64417b5f3e388bd8fa9d60fcebb5904169
asierhandball <asierhandballer@gmail.com>
2016-05-20 16:48:36 +0200

    Añadida linea que soluciona error de trello etiqueta abierta por adrian

--- app/src/main/java/redwinecorp/misvinos/MisGrupos.java
+++ app/src/main/java/redwinecorp/misvinos/MisGrupos.java
@@ -4,10 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -198,7 +196,10 @@ public class MisGrupos extends AppCompatActivity {
                     orden = -1;
                     fillData();
                     return true;
+                default:
+                        return super.onOptionsItemSelected(item);
             }
+
         }
         return super.onOptionsItemSelected(item);
     }
